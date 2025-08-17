90e3aea8e59045df8d093d4d2e28ea98dd91ea4d
[Abhiraj Vishwakarma] <[abhirajvishwakarma04@gmail.com]>
2025-08-17 20:14:03 +0530

    test

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
+@app.get("/")       #decorater
+def root():
     return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+@app.post("/createposts")
+def create_posts(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"new_post": f"tittle {payLoad['tittle']} content: {payLoad['content']}"}
