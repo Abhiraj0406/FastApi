d31241f39a5129eb88dc324bbb0619ea79d4b106
[Abhiraj Vishwakarma] <[abhirajvishwakarma04@gmail.com]>
2025-08-18 22:04:33 +0530

    Get Post by ID

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ class Post(BaseModel):
 
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id":1}, {"title": "fsvorite foods", "content": "I like pizza", "id": 2}]
 
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 @app.get("/")       #decorater
 def root():
     return {"message": "Hello World"}
@@ -37,3 +42,12 @@ def create_post(post: Post):
     post_dict['id'] = randrange(0, 100000)
     my_posts.append(post_dict)
     return {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    # print(id)
+    # return {"post_detail": f"here is post {id}"}
+    print(type(id))
+    post = find_post(id)
+    print(post)
+    return {"post_detail": post}
